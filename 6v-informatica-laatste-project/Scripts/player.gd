a593139e85fe07cc55f055794059b286f7c33cb1
Majorslegend <brent24925@gmail.com>
2026-02-25 18:57:18 +0100

    Brent push avomd 25-02-2026

--- 6v-informatica-laatste-project/Scripts/player.gd
+++ 6v-informatica-laatste-project/Scripts/player.gd
@@ -7,12 +7,16 @@ const JUMP_VELOCITY = -450.0
 
 var current_level = 0
 var level_scenes = [
-	
+	"res://Levels/main.tscn",
+	"res://Levels/node_2d.tscn"
 ]
 	
 func next_level():
-		current_level +=1
-	
+	current_level += 1
+	if current_level < level_scenes.size():
+		get_tree().change_scene_to_file(level_scenes[current_level])
+	else:
+		get_tree().change_scene_to_file("res://Levels/Main_menu.tscn")
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.

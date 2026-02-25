2a16782c0227449934dbd91856411d480ea11100
Avigyan2307 <avig2307@gmail.com>
2026-02-25 12:51:11 +0100

    Further update using temp assets

--- 6v-informatica-laatste-project/Scripts/player.gd
+++ 6v-informatica-laatste-project/Scripts/player.gd
@@ -3,24 +3,34 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -850.0
+const JUMP_VELOCITY = -450.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
+	if velocity.x > 1 or velocity.x <-1:
+		animated_sprite_2d.animation = "run"
+	else:
+		animated_sprite_2d.animation = "idle"
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		animated_sprite_2d.animation = "jump"
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	elif direction == -1.0:
+		animated_sprite_2d.flip_h = true
